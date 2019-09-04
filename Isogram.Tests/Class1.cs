a9811da580a037498dcf7e0204f8b5cb353ee5f7
rahul2106 <rahul.kmr2106@gmail.com>
2019-09-04 10:45:36 +0530

    Solution:

---
+++ Isogram.Tests/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using isogram_csharp_problem;
+
+namespace Isogram.Tests
+{
+    public class Class1
+    {
+
+        [Theory]
+        [InlineData("",true)]
+        [InlineData("isogram",true)]
+        [InlineData("eleven",false)]
+        [InlineData("subdermatoglyphic", true)]
+        [InlineData("Alphabet", true)]
+        [InlineData("thumbscrew-japingly", true)]
+        [InlineData("six-year-old", true)]
+        [InlineData("Q w E", true)]
+        [InlineData("QWERTYUIOPASDFGHJKLZXCVBNM - - -qwertyuiop  asdfghjkl zxcvbnm", true)]
+        [InlineData("QWERTYUIOPASDFGHJKLZXCVBNM - - -qwerrtyuiop  asdfghjkl zxcvbnm", false)]
+
+        public void IsIsogram(string value,bool y)
+        {
+            var result = Program.isIsogram(value);
+
+            Assert.Equal(result,y);
+        }
+    }
+}
---
+++ isogram-csharp-problem/isogram-csharp-problem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace isogram_csharp_problem
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+        }
+       public  static bool isIsogram(string str)
+        {
+           
+            int len = str.Length;
+
+            int []a = new int[200];
+            for (int i = 0; i < 200; i++)
+                a[i] = 0;
+
+            for (int i = 0; i < len - 1; i++)
+            {
+                if (!Char.IsLetter(str[i]))
+                    continue;
+                if (a[str[i]] == 0)
+                    a[str[i]] = 1;
+                else
+                    return false;
+            }
+            return true;
+        }
+    }
+}
